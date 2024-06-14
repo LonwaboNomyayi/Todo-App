c24faea49decee339aa4546c82ac499fcd5fc004
Lonwabo Nomyayi <lonwabonomyayi@gmail.com>
2024-06-14 12:16:20 +0200

    project files

---
+++ todo/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import SingleTodo
+
+
+
+class SingleTodoForm(forms.ModelForm):
+
+    class Meta:
+        model = SingleTodo
+        fields = ['title']
+
